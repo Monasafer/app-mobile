dcbd66c6f479f06f47d23971d6996360b7757598
Lysholm-Mariano <mariano.lysholm@gmail.com>
2021-05-03 13:33:36 -0300

    Update profitComponent.dart
    Cambie colorcito

--- lib/profitComponent.dart
+++ lib/profitComponent.dart
@@ -16,7 +16,7 @@ class _ProfitComponentState extends State<ProfitComponent> {
         child: Text(
           'Safe 78.000',
           textScaleFactor: 1.8,
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(color: Colors.white),
         ),
       ),
       decoration: BoxDecoration(
